8cafd14cb11f30ab8129705474ef7b8d513a0263
Mohamed Infaz <infaz9999@gmail.com>
2013-09-13 09:53:58 +0530

    Content Gravatar

---
+++ plugins/content/gravatar/Gravatar.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Authentication.joomla
+ *
+ * @copyright   Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+    defined('_JEXEC') or die; // Stopping Unauthorized access 
+/**
+ * Joomla Authentication plugin
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  Authentication.joomla
+ * @since       1.5
+ */
+    Class PlgContentGravatar extends JPlugin
+    {
+        
+        
+        
+        
+        
+    }
+?>
